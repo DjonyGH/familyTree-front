b784439aaabd128494f21199721ecdccc598ecf8
Evgeniy Kalimullin <counsil@mail.ru>
2022-11-19 11:13:17 +0700

    refactored component Person

--- src/store/reducers/tree/mock.ts
+++ src/store/reducers/tree/mock.ts
@@ -2,8 +2,9 @@ import { ITree } from '../../../types'
 
 export const tree: ITree = {
   id: 'qwqwq',
-  name: 'Петр',
-  surname: 'Иванов',
+  name: 'Наталья',
+  surname: 'Калимуллина (Фатеева)',
+  patronymic: 'Николаевна',
   birthday: '2000-10-10',
   createdAt: '2022-01-01',
   createdBy: 'Петр Иванов',
